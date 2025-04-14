306862b098edaeb7bfac43b1158028f2203ab985
Keyvan <keyvan.khezri@outlook.com>
2025-04-14 11:50:06 +0100

    Add project files.

---
+++ MathGame/Menu.cs
@@ -0,0 +1,57 @@
+﻿namespace MathGame;
+
+internal class Menu
+{
+    GameEngine engine = new();
+    internal void ShowMenu(string name, DateTime date)
+    {
+        Console.Clear();
+        Console.WriteLine($"Hello {name}. It's {date}. This is your math's game. That's great that you're working on improving yourself");
+        Console.WriteLine("Press any key to show menu");
+        Console.ReadLine();
+        Console.WriteLine("\n");
+
+        var isGameOn = true;
+
+        do
+        {
+            Console.Clear();
+            Console.WriteLine(@$"What game would you like to play today? Choose from the options below:
+V - View Previous Games
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+            Console.WriteLine("---------------------------------------------------");
+
+            var gameSelected = Console.ReadLine();
+
+            switch (gameSelected.Trim().ToLower())
+            {
+                case "v":
+                    Helpers.PrintGames();
+                    break;
+                case "a":
+                    engine.AdditionGame("Addition game");
+                    break;
+                case "s":
+                    engine.SubtractionnGame("Subtraction game");
+                    break;
+                case "m":
+                    engine.MultiplicationGame("Multiplication game");
+                    break;
+                case "d":
+                    engine.DivisionGame("Division game");
+                    break;
+                case "q":
+                    Console.WriteLine("Goodbye");
+                    isGameOn = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+        } while (isGameOn);
+    }
+}
